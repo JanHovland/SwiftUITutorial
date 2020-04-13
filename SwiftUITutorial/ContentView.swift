6cdc4cf96aa87b1bc7182fd3afcbaca98a25944a
JanHovland <jan.hovland@lyse.net>
2020-04-13 09:16:57 +0200

    Update

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -11,10 +11,10 @@ import WebKit
 import CloudKit
 
 var tutorials = [ Tutorial(title: "Getting Started With Combine",
-                           introduction: "Maybe you’re not yet ready to jump\ninto SwiftUI but you can still get started\nwith Combine. Here’s a gentle introduction to using \nCombine to validate user input.",
+                           introduction: "Maybe you’re not yet ready to jump\ninto SwiftUI but you can still get started\nwith Combine. Here’s a gentle introduction to using\nCombine to validate user input.",
                            url: "https://useyourloaf.com/blog/getting-started-with-combine/?utm_campaign=AppCoda%20Weekly&utm_medium=email&utm_source=Revue%20newsletter"),
                   Tutorial(title: "SwiftUI ButtonStyle",
-                           introduction: "SwiftUI makes it very easy to customize\n a button style. Learn how to create a reusable\n button style in SwiftUI.",
+                           introduction: "SwiftUI makes it very easy to customize\na button style. Learn how to create a reusable\nbutton style in SwiftUI.",
                            url:"https://sarunw.com/posts/swiftui-buttonstyle/?utm_campaign=AppCoda%20Weekly&utm_medium=email&utm_source=Revue%20newsletter")]
 
 struct TestView : NSViewRepresentable {
@@ -38,44 +38,18 @@ struct ContentView: View {
         NavigationView {
             List (tutorials) { tutorial in
                 NavigationLink(destination: TestView(tutorial: tutorial)) {
-                    VStack (alignment: .trailing) {
-                        VStack (alignment: .center) {
-                            Text(tutorial.title)
-                            Text(tutorial.introduction)
-                        }
+                    VStack (alignment: .leading, spacing: 10) {
+                        Text(tutorial.title)
+                            .font(Font.system(.headline))
+                        Text(tutorial.introduction)
+                            .padding(.leading, 15)
                     }
+                    .padding(.top, 5)
                 }
             }
         }
         .listStyle(SidebarListStyle())
-        /// .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
-//import SwiftUI
-//
-//struct ContentView: View {
-//    var body: some View {
-//        NavigationView {
-//            List {
-//                ForEach(1 ... 10, id: \.self) { index in
-//                    NavigationLink(destination: DetailView(value: index)) {
-//                        Text("Link \(index)")
-//                    }
-//                }
-//
-//            }
-//        }
-//        .listStyle(SidebarListStyle())
-//    }
-//}
-//
-//
-//struct DetailView: View {
-//    var value: Int
-//
-//    var body: some View {
-//        Text("Detail Link for index " + String(value))
-//            .frame(maxWidth: .infinity, maxHeight: .infinity)
-//    }
-//}
